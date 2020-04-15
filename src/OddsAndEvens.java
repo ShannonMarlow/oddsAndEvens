58f1421d042b3fb5b299b05fe916d433f8ee32b1
Shannon Marlow <shannonmarlow13@gmail.com>
2020-04-15 10:55:36 +0100

    third commit

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -4,6 +4,16 @@ public class OddsAndEvens {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Let's play a game called \"Odds and Evens\" ");
+        System.out.println("\n---------------------------------------------\n");
+        System.out.print("\nHere are the rules: \n" +
+                "The game is similar to rock paper scissors. It is played between two players, \n" +
+                "in your version it will be you versus the computer. \n" +
+                "Each player will choose either \"odds\" or \"evens\", since you’re playing the computer you will get first pick. \n" +
+                "Once you have chosen your side, you each choose a number of fingers to play- 0 to 5. \n" +
+                "The winner is determined by whether the sum of your fingers is odd or even (depending on what you chose).\n" +
+                "For example, let’s say you choose \"evens\". That means the computer gets \"odds\". \n" +
+                "You choose to play 2 fingers and the computer plays 3. 2 + 3 = 5, which is odd so the computer wins.\n");
+        System.out.println("\n---------------------------------------------\n");
         System.out.print("What is your name? ");
         String name = input.nextLine();
         System.out.print("Hi " + name + " which do you choose? (O)dds or (E)vens? ");
@@ -26,9 +36,9 @@ public class OddsAndEvens {
         if (sum % 2 == 0) {
             System.out.print(sum + " is even ");
             if(team.equalsIgnoreCase("o")){
-                System.out.print(name + " wins, Well Done!");
-            }else{
                 System.out.print(" computer wins, better luck next time");
+            }else{
+                System.out.print(name + " wins, Well Done!");
             }
         } else {
             System.out.print(sum + " is odd ");
